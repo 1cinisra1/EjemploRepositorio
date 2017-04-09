31ecf34ba7575e41fc46ed74b73b886e3f530c48
Andres <sanandres_021188@hotmail.com>
2017-04-09 03:14:24 -0500

    2017-04-09/3:14

--- Thesis/WebApplication/Controllers/EmpresaController.cs
+++ Thesis/WebApplication/Controllers/EmpresaController.cs
@@ -24,7 +24,6 @@ namespace WebApplication.Controllers
             e1.ruc = "0999999999";
             lista.Add(e1);
 
-            List<EmpresaAccount> lista1 = new List<EmpresaAccount>();
             EmpresaAccount e2 = new EmpresaAccount();
             e2.codigo = 2;
             e2.nombre = "Machinetech";
@@ -34,7 +33,6 @@ namespace WebApplication.Controllers
             e2.ruc = "0999999999";
             lista.Add(e2);
 
-            List<EmpresaAccount> lista2 = new List<EmpresaAccount>();
             EmpresaAccount e3 = new EmpresaAccount();
             e3.codigo = 3;
             e3.nombre = "PSI";
@@ -43,6 +41,69 @@ namespace WebApplication.Controllers
             e3.telefono = "042627748";
             e3.ruc = "0999999999";
             lista.Add(e3);
+
+            EmpresaAccount e4 = new EmpresaAccount();
+            e4.codigo = 4;
+            e4.nombre = "PSI";
+            e4.direccion = "Kennedy";
+            e4.ciudad = "Guayaquil";
+            e4.telefono = "042627748";
+            e4.ruc = "0999999999";
+            lista.Add(e4);
+
+            EmpresaAccount e5 = new EmpresaAccount();
+            e5.codigo = 5;
+            e5.nombre = "PSI";
+            e5.direccion = "Kennedy";
+            e5.ciudad = "Guayaquil";
+            e5.telefono = "042627748";
+            e5.ruc = "0999999999";
+            lista.Add(e5);
+
+            EmpresaAccount e6 = new EmpresaAccount();
+            e6.codigo = 6;
+            e6.nombre = "PSI";
+            e6.direccion = "Kennedy";
+            e6.ciudad = "Guayaquil";
+            e6.telefono = "042627748";
+            e6.ruc = "0999999999";
+            lista.Add(e6);
+
+            EmpresaAccount e7 = new EmpresaAccount();
+            e7.codigo = 7;
+            e7.nombre = "PSI";
+            e7.direccion = "Kennedy";
+            e7.ciudad = "Guayaquil";
+            e7.telefono = "042627748";
+            e7.ruc = "0999999999";
+            lista.Add(e7);
+
+            EmpresaAccount e8 = new EmpresaAccount();
+            e8.codigo = 8;
+            e8.nombre = "PSI";
+            e8.direccion = "Kennedy";
+            e8.ciudad = "Guayaquil";
+            e8.telefono = "042627748";
+            e8.ruc = "0999999999";
+            lista.Add(e8);
+
+            EmpresaAccount e9 = new EmpresaAccount();
+            e9.codigo = 9;
+            e9.nombre = "PSI";
+            e9.direccion = "Kennedy";
+            e9.ciudad = "Guayaquil";
+            e9.telefono = "042627748";
+            e9.ruc = "0999999999";
+            lista.Add(e9);
+
+            EmpresaAccount e10 = new EmpresaAccount();
+            e10.codigo = 10;
+            e10.nombre = "PSI";
+            e10.direccion = "Kennedy";
+            e10.ciudad = "Guayaquil";
+            e10.telefono = "042627748";
+            e10.ruc = "0999999999";
+            lista.Add(e10);
 
             return View(lista);
         }
