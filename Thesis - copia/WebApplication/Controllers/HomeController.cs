339daa7573e8ba8ced7ec102645031392e1c431a
Andres <sanandres_021188@hotmail.com>
2017-04-23 18:22:02 -0500

    v2

---
+++ Thesis - copia/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult DashboardV1()
+        {
+            if (Session["LogedUserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+        public ActionResult DashboardV2()
+        {
+            return View();
+        }
+    }
+}
