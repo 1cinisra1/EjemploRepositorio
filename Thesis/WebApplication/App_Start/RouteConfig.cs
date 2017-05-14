c292b25ddbd8099b7fbad3cc1a118e4fae7ae35c
1cinisra1 <ip1cinisra1@hotmail.com>
2017-05-14 01:16:08 -0500

    Pagina principal añadida

--- Thesis/WebApplication/App_Start/RouteConfig.cs
+++ Thesis/WebApplication/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace WebApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "MyAccount", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                 
             );
 
