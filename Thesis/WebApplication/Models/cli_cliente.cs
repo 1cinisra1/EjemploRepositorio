a5d211c9fec6a1f6faab5d2dc3abdff29f50b43e
Andres <sanandres_021188@hotmail.com>
2017-05-14 16:53:44 -0500

    script

--- Thesis/WebApplication/Models/cli_cliente.cs
+++ Thesis/WebApplication/Models/cli_cliente.cs
@@ -11,6 +11,7 @@ namespace WebApplication.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class cli_cliente
     {
@@ -20,11 +21,25 @@ namespace WebApplication.Models
         }
     
         public int idCli_Cliente { get; set; }
+
+        [Required( ErrorMessage = "El campo no puede estar en blanco.")]
+        [StringLength(100, ErrorMessage = "El campo debe contener minimo {2} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
         public string Cli_Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo no puede estar en blanco.")]
+        [StringLength(100, ErrorMessage = "El campo debe contener minimo {2} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Range(0, Int32.MaxValue, ErrorMessage = "El valor {0} debe ser numérico.")]
         public string Cli_Ruc { get; set; }
+
         public string Cli_Direccion { get; set; }
         public string Cli_Tel { get; set; }
         public string Cli_Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El campo no puede estar en blanco.")]
+        [StringLength(100, ErrorMessage = "El campo debe contener minimo {2} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
         public string Cli_RSocial { get; set; }
         public string Cli_NombContacto { get; set; }
     
